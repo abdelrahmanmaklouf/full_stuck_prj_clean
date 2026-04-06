90b593804e30425c2527c03273e4f0dd268a85f6
Abdelrahman Ashraf <ashrafbota29@gmail.com>
2026-04-06 07:41:59 +0200

    fix App import case sensitivity

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
+import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
